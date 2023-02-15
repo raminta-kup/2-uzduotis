965d26e5bf5b7b25ff6b313372af0d6cf60b6b33
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-02-15 19:51:05 +0200

    count clicks function

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+const btn = document.querySelector("#btn__element");
+const btnState = document.querySelector("#btn__state");
+let clicks = 0;
+
+function countPresses() {
+    clicks += 1;
+    btnState.innerHTML = clicks;
+}
+
+btn.addEventListener("click", countPresses);
